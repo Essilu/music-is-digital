a8e5a2402db3294b8441d67449c12d73907bfe55
Elliot Maisl <elliot.maisl@gmail.com>
2020-12-08 12:24:05 +0100

    Add a sound function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import re
-
+from time import sleep
+import numpy as np
+import simpleaudio as sa
 class Note:
     def __init__(self, raw_input) -> None:
         self.raw_input = raw_input # SOLcp
@@ -44,7 +46,35 @@ class Note:
         elif self.parsed_data[1] == 'c':
             duration = 125
         point_duration = duration / 2 if has_point else 0
-        return duration + point_duration
+        return (duration + point_duration) / 1000
+
+    def play(self):
+        # Get timesteps for each sample, "duration" is note duration in seconds
+        sample_rate = 44100
+        t = np.linspace(0, self.duration, int(self.duration * sample_rate), False)
+        # Generate sine wave tone
+        tone = np.sin(self.frequency * t * 6 * np.pi)
+        # Normalize to 24−bit range
+        tone *= 8388607 / np.max(np.abs(tone))
+        # Convert to 32−bit data
+        tone = tone.astype(np.int32)
+
+        # Convert from 32−bit to 24−bit by building a new byte buffer ,
+        # Skipping every fourth bit
+        # Note: this also works for 2−channel audio
+        i = 0
+        byte_array = [ ]
+        for b in tone.tobytes():
+            if i % 4 != 3:
+                byte_array.append(b)
+            i += 1
+
+        audio = bytearray(byte_array)
+        # Start playback
+        play_obj = sa.play_buffer(audio, 1, 3, sample_rate)
+        # Wait for playback to finish before exiting
+        play_obj.wait_done()
+
 
 
 file = open("./assets/partitions.txt", "r")
@@ -75,3 +105,6 @@ def get_notes_from_line(line):
 # Get the first line, remove the last 2 characters (\n), and remove spaces
 curr_line = lines[1][:-1].replace(' ', '')
 raw_notes = get_notes_from_line(curr_line)
+for raw_note in raw_notes:
+    note = Note(raw_note)
+    note.play()
