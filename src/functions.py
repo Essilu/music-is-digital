06314915d94fa368893d1058190f00bfba8ba6b5
Elliot Maisl <elliot.maisl@gmail.com>
2020-12-08 12:58:01 +0100

    Reorganize files

---
+++ src/functions.py
@@ -0,0 +1,26 @@
+def get_notes_from_line(line):
+    """
+    Takes a line as a string, and gives back an array of note.
+
+    We didn't just splitted at each spaces because we wanted the "p" to be part of
+    the note it succeeds, to increase directly the duration of the note.
+    Hence, we split the string each time we encounter an uppercase character, preceeded
+    with a lowercase character, meaning we found the beggining of a new note.
+    """
+    last_char_is_upper = True
+    raw_notes_list = []
+    current_note = ""
+
+    for char in line:
+        if not last_char_is_upper and char.isupper():
+            last_char_is_upper = True
+            raw_notes_list.append(current_note)
+            current_note = char
+        else:
+            if char.isupper():
+                last_char_is_upper = True
+            else:
+                last_char_is_upper = False
+            current_note += char
+
+    return raw_notes_list
--- src/main.py
+++ src/main.py
@@ -1,34 +1,17 @@
-from note import Note
-
+import classes
+import functions
 
 file = open("./assets/partitions.txt", "r")
 lines = file.readlines()
 file.close()
 
 
-def get_notes_from_line(line):
-    last_char_is_upper = True
-    raw_notes_list = []
-    current_note = ""
-
-    for char in line:
-        if not last_char_is_upper and char.isupper():
-            last_char_is_upper = True
-            raw_notes_list.append(current_note)
-            current_note = char
-        else:
-            if char.isupper():
-                last_char_is_upper = True
-            else:
-                last_char_is_upper = False
-            current_note += char
-
-    return raw_notes_list
-
+music_index = int(input("Enter a music to play (1, 3, 5, 7...)"))
 
 # Get the first line, remove the last 2 characters (\n), and remove spaces
-curr_line = lines[3][:-1].replace(' ', '')
-raw_notes = get_notes_from_line(curr_line)
+curr_line = lines[music_index][:-1].replace(' ', '')
+raw_notes = functions.get_notes_from_line(curr_line)
+
 for raw_note in raw_notes:
-    note = Note(raw_note)
+    note = classes.Note(raw_note)
     note.play()
